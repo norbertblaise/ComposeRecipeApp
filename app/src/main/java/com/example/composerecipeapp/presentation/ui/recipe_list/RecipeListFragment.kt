e54275b5b2637ea811069fc040611a50782862a9
norbertblaise <lukwayi@gmail.com>
2022-05-10 23:08:00 +0300

    repository added

--- app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -5,20 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
-import com.example.composerecipeapp.R
+import androidx.lifecycle.lifecycleScope
+import com.example.composerecipeapp.presentation.components.FoodCategoryChip
+import com.example.composerecipeapp.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class RecipeListFragment : Fragment() {
@@ -31,19 +46,85 @@ class RecipeListFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 val recipes = viewModel.recipes.value
+                val query =
+                    remember { viewModel.query.value }
 
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        "Recipe List",
-                        style = TextStyle(
-                            fontSize = 21.sp,
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = {
-                        findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment)
-                    }) {
-                        Text("To Recipe Fragment")
+                val selectedCategory = viewModel.selectedCategory.value
+                Column {
+                    Surface(
+                        modifier = Modifier.fillMaxWidth(),
+                        color = Color.White,
+                        elevation = 8.dp,
+                    ) {
+                        Column {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                            ) {
+                                TextField(
+                                    modifier = Modifier
+                                        .fillMaxWidth(.9f)
+                                        .padding(8.dp),
+                                    value = query,
+                                    onValueChange = {
+                                        viewModel.onQueryChanged(it)
+                                    },
+                                    label = {
+                                        Text(text = "Search")
+                                    },
+                                    keyboardOptions = KeyboardOptions(
+                                        keyboardType = KeyboardType.Text,
+                                        imeAction = ImeAction.Done,
+                                    ),
+                                    leadingIcon = {
+                                        Icon(Icons.Filled.Search)
+                                    },
+                                    onImeActionPerformed = { action, softKeyboardController ->
+                                        if (action == ImeAction.Done) {
+                                            viewModel.newSearch()
+                                            softKeyboardController?.hideSoftwareKeyboard()
+                                        }
+                                    },
+                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                                    backgroundColor = MaterialTheme.colors.surface
+                                )
+
+                            }
+                            val scrollState = rememberLazyListState()
+
+                            LazyRow(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 8.dp, bottom = 8.dp),
+                                state = scrollState
+                            ) {
+                                lifecycleScope.launch { scrollState.scrollToItem(getAllFoodCategories().indexOf(selectedCategory)) }
+
+                                for (category in getAllFoodCategories()) {
+                                    FoodCategoryChip(
+                                        category = category.value,
+                                        isSelected = selectedCategory == category,
+                                        onSelectedCategoryChanged = {
+                                            viewModel.onSelectedCategoryChanged(it)
+                                            viewModel.onCategoryChangePosition(
+                                                getAllFoodCategories().indexOf(selectedCategory))
+                                        },
+                                        onExecuteSearch =
+                                        viewModel::newSearch
+
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    LazyColumn {
+                        itemsIndexed(
+                            items = recipes
+                        ) { index, recipe ->
+                            RecipeCard(
+                                recipe = recipe,
+                                onClick = {}
+                            )
+                        }
                     }
 
                 }
--- app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/composerecipeapp/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -21,17 +21,19 @@ constructor(
 
     val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
     val query = mutableStateOf("")
+    val selectedCategory: MutableState<FoodCategory?> = mutableStateOf(null)
+    var categoryScrollPosition: Int = 0
 
     init {
-        newSearch(query.value)
+        newSearch()
     }
 
-    fun newSearch(query: String) {
+    fun newSearch() {
         viewModelScope.launch {
             val result = repository.search(
                 token = token,
                 page = 1,
-                query = query
+                query = query.value
             )
             recipes.value = result
         }
@@ -39,4 +41,16 @@ constructor(
     fun onQueryChanged(query: String){
         this.query.value = query
     }
+
+    fun onSelectedCategoryChanged(category:String){
+        val newCategory = getFoodCategory(category)
+
+        selectedCategory.value = newCategory
+        onQueryChanged(category)
+    }
+
+    fun onCategoryChangePosition(position: Int){
+        categoryScrollPosition = position
+    }
+
 }
