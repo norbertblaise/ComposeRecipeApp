d25ffef5854da019407d453269bc697da38c9051
norbertblaise <lukwayi@gmail.com>
2022-05-17 22:48:31 +0300

    PulsingDemo.kt added and updated with latest animations set pulsing heart added

--- app/src/main/java/com/example/composerecipeapp/presentation/components/PulsingDemo.kt
+++ app/src/main/java/com/example/composerecipeapp/presentation/components/PulsingDemo.kt
@@ -2,11 +2,14 @@ package com.example.composerecipeapp.presentation.components
 
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.unit.dp
@@ -18,7 +21,7 @@ fun PulsingDemo() {
 
     val infiniteTransition = rememberInfiniteTransition()
 
-    val floatValues by infiniteTransition.animateFloat(
+    val pulseMagnitude by infiniteTransition.animateFloat(
         initialValue = 40f,
         targetValue = 50f,
         animationSpec = infiniteRepeatable(
@@ -26,13 +29,28 @@ fun PulsingDemo() {
             repeatMode = RepeatMode.Restart
         ),
     )
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(55.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .height(pulseMagnitude.dp)
+                .width(pulseMagnitude.dp),
+            imageVector = Icons.Filled.Favorite,
+            contentDescription = null
+        )
+    }
     Canvas(
         modifier = Modifier
             .fillMaxWidth()
             .height(55.dp)
     ) {
         drawCircle(
-            radius = floatValues,
+            radius = pulseMagnitude,
             brush = SolidColor(color)
         )
     }
