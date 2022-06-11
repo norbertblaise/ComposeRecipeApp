f647160a39260293a67620f3371010eff960971c
norbertblaise <lukwayi@gmail.com>
2022-06-11 22:50:28 +0300

    PulsingDemo.kt added and updated with latest animations set pulsing heart added

--- app/src/main/java/com/example/composerecipeapp/util/ImageUtils.kt
+++ app/src/main/java/com/example/composerecipeapp/util/ImageUtils.kt
@@ -46,3 +46,23 @@ fun LoadPicture(url: String, @DrawableRes defaultImage: Int): MutableState<Bitma
 
     return bitmapState
 }
+
+
+@Composable
+fun LoadPicture(@DrawableRes drawable: Int): MutableState<Bitmap?> {
+    val bitmapState: MutableState<Bitmap?> = remember { mutableStateOf(null) }
+
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(drawable)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                bitmapState.value = resource
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+            }
+
+        })
+    return bitmapState
+}
